e9e4803042afb99982982b513eaf689f0ffece4e
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-08 20:58:39 +0100

    final update

--- app.js
+++ app.js
@@ -6,94 +6,117 @@ var currPlayer = playerRed;
 
 var gameOver = false;
 var board;
-var currColumns;
 
 var rows = 6;
 var columns = 7;
+var currColumns = []; //keeps track of which row each column is at.
 
-
-window.onload = function(){
+window.onload = function() {
     setGame();
 }
 
-function setGame(){
+function setGame() {
     board = [];
     currColumns = [5, 5, 5, 5, 5, 5, 5];
-    for(let r = 0; r < rows; r++){
-        let row = [];
-        for(let c = 0; c <columns; c++){
-            row.push('');
 
-            let tile = document.createElement('div')
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
+            // HTML
+            let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
-            tile.addEventListener("click", setPiece)
-            document.getElementById("board").append(tile)
-
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
         }
-        board.push(row)
+        board.push(row);
     }
 }
 
-
-function setPiece(){
-    if (gameOver){
-        return
+function setPiece() {
+    if (gameOver) {
+        return;
     }
+
+    //get coords of that tile clicked
     let coords = this.id.split("-");
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
-    r = currColumns[c];
-    if (r < 0 ){
-        return
+    // figure out which row the current column should be on
+    r = currColumns[c]; 
+
+    if (r < 0) { // board[r][c] != ' '
+        return;
     }
 
-
-
-    board[r][c] = currPlayer;
+    board[r][c] = currPlayer; //update JS board
     let tile = document.getElementById(r.toString() + "-" + c.toString());
-    if (currPlayer == playerRed){
+    if (currPlayer == playerRed) {
         tile.classList.add("red-piece");
-        currPlayer = playerYellow
+        currPlayer = playerYellow;
     }
-    else{
+    else {
         tile.classList.add("yellow-piece");
-        currPlayer = playerRed
-
+        currPlayer = playerRed;
     }
-    r -= 1;
-    currColumns[c] = r;
 
-        checkWinner()
+    r -= 1; //update the row height for that column
+    currColumns[c] = r; //update the array
+
+    checkWinner();
 }
 
-function checkWinner(){
-    // checking winner horizontally
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns - 3; c++){
-           if (board[r][c] != ' ') {
-               if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
-                   setWinner(r, c);
-                   return;
-               }
-           }
+function checkWinner() {
+     // horizontal
+     for (let r = 0; r < rows; r++) {
+         for (let c = 0; c < columns - 3; c++){
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+         }
+    }
+
+    // vertical
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
         }
-   }
+    }
 
-   // checking winner vertically
+    // anti diagonal
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
 
-   for (let c = 0; c < columns; c++) {
-       for (let r = 0; r < rows - 3; r++) {
-           if (board[r][c] != ' ') {
-               if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
-                   setWinner(r, c);
-                   return;
-               }
-           }
-       }
-   }
-
+    // diagonal
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
 }
 
 function setWinner(r, c) {
