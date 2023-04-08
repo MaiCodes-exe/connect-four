ca1ca212e55884abde2d23c2ffcd144e1c7a47ad
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-08 20:27:59 +0100

    added game logic

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var currPlayer = playerRed;
 
 var gameOver = false;
 var board;
+var currColumns;
 
 var rows = 6;
 var colums = 7;
+
 
 window.onload = function(){
     setGame();
@@ -16,6 +18,7 @@ window.onload = function(){
 
 function setGame(){
     board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
     for(let r = 0; r < rows; r++){
         let row = [];
         for(let c = 0; c <colums; c++){
@@ -40,8 +43,16 @@ function setPiece(){
     let coords = this.id.split("-");
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
+
+    r = currColumns[c];
+    if (r < 0 ){
+        return
+    }
+
+
+
     board[r][c] = currPlayer;
-    let tile = this;
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
     if (currPlayer == playerRed){
         tile.classList.add("red-piece");
         currPlayer = playerYellow
@@ -51,5 +62,33 @@ function setPiece(){
         currPlayer = playerRed
 
     }
+    r -= 1;
+    currColumns[c] = r;
 
+        checkWinner()
+}
+
+function checkWinner(){
+
+    for (let r = 0; r <rows; r++){
+        for(let c = 0; c < colums - 3; c++){
+            if (board[r][c] != ' '){
+                if (board[r][c] == board[r] [c+1] && board[r] [c+1] && board[r] [c+2] && board[r] [c+2] == board[r] [c+3]){
+                    setWinner(r,c);
+                    return
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r,c){
+    let winner = document.getElementById("winner");
+    if (board[r][c] == playerRed){
+        winner.innerHTML= "Red Wins"
+    }
+    else{
+        winner.innerHTML= "Yellow Wins"
+    }
+    gameOver = true
 }
